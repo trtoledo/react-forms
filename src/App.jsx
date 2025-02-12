70582a684d091db28f79629e3b4086515a586529
Tomas Toledo <trtoledo1977@gmail.com>
2025-02-12 19:46:22 -0300

    signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+import './App.css'
+export default function App() {
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+}
+
+
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import './App.css'
+
 
 // function App() {
 //   const [count, setCount] = useState(0)
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export default function SignUpForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Hello 👋");
+  }
+
+  return (
+    <>
+      <h2>Sign Up!</h2>;
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password: <input value={password} onChange={(e) => setPassword(e.target.value)} /> 
+        </label>
+        <button>Submit</button>
+      </form>
+      
+    </>
+  );
+}
